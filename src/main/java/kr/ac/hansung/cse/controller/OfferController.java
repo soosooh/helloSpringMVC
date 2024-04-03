8caacaaa85a72ee22f8e92ea4a607b41ce86b608
soohee Jang <shjang0820@hansung.ac.kr>
2024-04-03 13:31:23 +0900

    add spring web form functionality

--- src/main/java/kr/ac/hansung/cse/controller/OfferController.java
+++ src/main/java/kr/ac/hansung/cse/controller/OfferController.java
@@ -5,8 +5,13 @@ import kr.ac.hansung.cse.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
+import java.sql.SQLOutput;
 import java.util.List;
 
 @Controller
@@ -24,4 +29,37 @@ public class OfferController {
         return "offers";
 
     }
+
+    @GetMapping("/createoffer")
+    public String createOffer(Model model){
+
+        model.addAttribute("offer", new Offer());
+
+        return "createoffer";
+    }
+
+    @PostMapping("/docreate")
+    public String doCreate(Model model, @Valid Offer offer, BindingResult result){
+        //valid 사용 시 사용자가 입력한 값들 검증해줌 -> 검증한 것들은 BindingResult에 넣기
+
+//        System.out.println(offer);
+        if(result.hasErrors()){
+            System.out.println("== Form data does not validated ==");
+
+            List<ObjectError> errors = result.getAllErrors();
+
+            for (ObjectError error:errors){
+                System.out.println(error.getDefaultMessage());
+            }
+
+            return "createoffer";
+        }
+
+        //Controller -> Service -> Dao
+
+        offerService.insert(offer);
+
+        return "offercreated";
+    }
+
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -8,9 +8,13 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>$Title$</title>
   </head>
   <body>
-  <p> <a href="${pageContext.request.contextPath}/offers">Show current offers</a></p>
+    <p> <a href="${pageContext.request.contextPath}/offers">Show current offers</a></p>
+    <p> <a href="${pageContext.request.contextPath}/createoffer">Add a new offer</a></p>
   </body>
 </html>
